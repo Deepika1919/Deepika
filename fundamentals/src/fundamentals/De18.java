8495629378ec3f92631aec3f82055d31ec6ed220
hp <hp@LAPTOP-DGQFM649>
2019-06-26 10:05:08 +0530

    Deepika

---
+++ fundamentals/src/fundamentals/De18.java
@@ -0,0 +1,13 @@
+package fundamentals;
+
+public class De18 {
+	public static void main(String args[]) {
+		char s='t';
+		if((s>='A' && s<='Z') ||(s>='a' && s<='z'))
+			System.out.println("Alphabet");
+		else if(s>='0' && s<='9')
+			System.out.println("Digit");
+				
+	}
+
+}
